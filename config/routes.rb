3f35d720ba3f5bf76d4ba5fed070f16dbbcd3a85
katsolenkova888 <kat.solenkova.tues19@gmail.com>
2017-10-10 23:13:37 +0300

    ruby

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
 
   
   match '/sums' =>'csv#sums',via: :post
-  match '/filters' =>'tasks#filters',via: :post
-  #match '/intervals' =>'tasks#intervals',via: :post
+  match '/filters' =>'csv#filters',via: :post
+  match '/calc' =>'csv#intervals',via: :post
   #match '/lin_regressions' =>'tasks#lin_regressions',via: :post
   
   
